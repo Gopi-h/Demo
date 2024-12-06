67ce7017ad07c6c131924a3540e3184f65053b71
gopi <gopithokala434@gmail.com>
2024-12-06 18:49:46 +0530

    V1.0

---
+++ src/main/java/com/gv/Hypernet/App.java
@@ -0,0 +1,13 @@
+package com.gv.Hypernet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/gv/Hypernet/AppTest.java
@@ -0,0 +1,20 @@
+package com.gv.Hypernet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
